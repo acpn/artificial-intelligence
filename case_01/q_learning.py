e740e34fe6696be0df29efbd146f18b97c81009c
acpn <ant.pnasc@gmail.com>
2019-08-05 23:19:49 -0300

    First commit

---
+++ case_01/q_learning.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# Artificial Intelligence for Business
+# Optimizing Warehouse Flows with Q-Learning
+
+# Libraries
+import numpy as np
+
+# Setting the parameters gamma and alpha for the Q-Learning
+gamma = 0.7
+alpha = 0.9
+
+# Part 1 - Defining the environment
+
+#     Step 1 - Defining the States
+location_to_state = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
+}
+
+#     Step 2 - Defining the Actions
+actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+#     Step 3 - Defining the Rewards
+
+
+# Part 2 - Building the A.I solution with Q-Learning
+# Part 3 - Going to Production
