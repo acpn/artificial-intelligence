a13c9677fd12d6d8ef1fcaf4026648e3a0e4f5ef
acpn <ant.pnasc@gmail.com>
2019-12-14 14:00:08 -0300

    Part 01 completed.

--- case_01/q_learning.py
+++ case_01/q_learning.py
@@ -48,27 +48,27 @@ R = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 # Part 2 - Building the A.I solution with Q-Learning
 
-#     Step 1 - Initializing the Q-Values
-Q = np.array(np.zeros([12, 12]))
-
-#     Step 2 - Implementing the Q-Learning process
-for i in range(1000):
-    current_state = np.random.randint(0, 12)
-    playable_actions = []
-    for j in range(12):
-        if R[current_state, j] > 0:
-            playable_actions.append(j)
-    next_state = np.random.choice(playable_actions) 
-    TD = R[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state] 
-    Q[current_state, next_state] += alpha * TD
-
-# Part 3 - Going to Production
-
 # Making a mapping from the states to the locations
 state_to_location = {state: location for location, state in location_to_state.items()}
 
 # Making the final function that will return the optimal route
 def route(starting_location, ending_location):
+    R_new = np.copy(R)
+    ending_state = location_to_state[ending_location]
+    R_new[ending_state, ending_state] = 1000
+    
+    # Q-Learn implementation
+    Q = np.array(np.zeros([12, 12]))
+    for i in range(1000):
+        current_state = np.random.randint(0, 12)
+        playable_actions = []
+        for j in range(12):
+            if R_new[current_state, j] > 0:
+                playable_actions.append(j)
+        next_state = np.random.choice(playable_actions) 
+        TD = R_new[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state] 
+        Q[current_state, next_state] += alpha * TD
+
     route = [starting_location]
     next_location = starting_location
     while (next_location != ending_location):
@@ -77,6 +77,18 @@ def route(starting_location, ending_location):
         next_location = state_to_location[next_state]
         route.append(next_location)
         starting_location = next_location
+    
+    print("Matrix R_new:")
+    print(R_new)
+
     return route
 
-print(route('E', 'G'))
+def best_route(starting_location, intermediary_location, ending_location):
+    return route(starting_location, intermediary_location) + route(intermediary_location, ending_location)[1:] # avoid duplicate location
+
+print("Route:")
+print(best_route('E', 'F', 'G'))
+
+print("Matrix R:")
+print(R)
+
